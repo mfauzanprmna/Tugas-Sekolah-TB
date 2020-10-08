82d98482c7fe1c2c1e6c67756c84a4586daf6b15
unknown <mfauzanpermana12@gmail.com>
2020-10-08 22:51:20 +0700

    Perbaikan Tampilan Web

--- Tugas Transaksi/script.js
+++ Tugas Transaksi/script.js
@@ -7,29 +7,67 @@ var waktu = [];
 var harga = [];
 var tampil = document.getElementById('tampilan');
 
+function ps2() {
+    date = new Date();
+    jam = date.getHours();
+    menit = date.getMinutes();
+    document.getElementById('jenis').value = "Ps 2";
+    document.getElementById('harga').value = 5000;
+    document.getElementById('awal').value = jam + " : " + menit;
+}
+
+function ps3() {
+    date = new Date();
+    jam = date.getHours();
+    menit = date.getMinutes();
+    document.getElementById('jenis').value = "Ps 3";
+    document.getElementById('harga').value = 10000;
+    document.getElementById('awal').value = jam + " : " + menit;
+}
+
+function ps4() {
+    date = new Date();
+    jam = date.getHours();
+    menit = date.getMinutes();
+    document.getElementById('jenis').value = "Ps 4";
+    document.getElementById('harga').value = 13000;
+    document.getElementById('awal').value = jam + " : " + menit;
+}
+
 function show() {
     tampil.innerHTML = '';
     for (i = 0; i < nama.length; i++) {
 
         var waktusewa = Number(waktu[i]);
-        if (waktusewa > 4) {
+        if (waktusewa > 10) {
             var total = Number(waktu[i]) + 1;
+            var hargasewa = (Number(harga[i]) * Number(waktu[i])) - (Number(harga[i]) * Number(waktu[i]) * Number(0.2))
+        } else if (waktusewa > 4) {
+            var total = Number(waktu[i]) + 1;
+            var hargasewa = (Number(harga[i]) * Number(waktu[i])) - (Number(harga[i]) * Number(waktu[i]) * Number(0.1))
         } else {
             var total = Number(waktu[i]);
+            var hargasewa = Number(harga[i]) * Number(waktu[i]);
         }
 
+        const RP = "Rp. "
         const SR = "SR - "
         const jam = " jam";
         const nomor = i + 1;
         tampil.innerHTML += '<tr>' +
             '<td>' + nomor + '</td>' +
-            '<td>' + kode[i] + SR + '</td>' +
+            '<td>' + SR + kode[i] + '</td>' +
             '<td>' + nama[i] + '</td>' +
             '<td>' + jenis[i] + '</td>' +
             '<td>' + awal[i] + '</td>' +
             '<td>' + kembali[i] + '</td>' +
-            '<td>' + total + +'</td>' +
-            '<td>' + harga[i] * waktu[i] + '</td>' +
+            '<td>' + total + jam + '</td>' +
+            '<td>' + RP + hargasewa + '</td>' +
+            '<td>' +
+            '<button class="btn edit">Edit</button>' +
+            '<button class="btn hapus">Hapus</button>' +
+            '<button class="btn details">Details</button>' +
+            '</td>' +
             '</tr>'
     }
 }
@@ -63,23 +101,23 @@ function add() {
     document.getElementById('harga').value = '';
 }
 
-function hitungtotal() {
-    var kodes = 0.0;
-    var nama = (document.form.nama.value);
-    var sewa = parseFloat(document.form.jam.value);
-    var ht = 0.0;
-    var jenis = (document.form.jenis.value);
-    var awal = (document.form.awal.value);
-    var kembali = (document.form.kembali.value);
+function edit(id) {
 
-    if (jenis == "Ps 2") {
+}
+
+function hitungtotal() {
+
+    var tujuan = document.getElementById('jenis').value;
+    var ht = 0.0
+    if (tujuan == "") {
+        ht = 0;
+    } else if (tujuan == "Ps 2") {
         ht = 5000;
-    } else if (jenis == "Ps 3") {
+    } else if (tujuan == "Ps 3") {
         ht = 10000;
     } else {
         ht = 13000;
     }
-    harga.value = ht;
 
-    document.form.harga.value = eval(ht);
+    document.getElementById('harga').value = ht;
 }
